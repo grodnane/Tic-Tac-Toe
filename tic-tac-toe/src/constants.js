458b740f2c14d18f60776230ae4a7f49d8303471
grodnane <gustavodaniel.rod@gmail.com>
2023-06-11 14:03:38 -0300

    change on simbols and css

--- tic-tac-toe/src/constants.js
+++ tic-tac-toe/src/constants.js
@@ -1,8 +1,10 @@
 export const TURNS = {
-    X: '×',
-    O: 'o'
+    X: '❌',
+    O: '⚪'
   }
 export const winnerCondition = [
     [0,1,2],[0,3,6],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
   ]
 
+
+export const theme = { ligth:"",dark:"#242424"}
